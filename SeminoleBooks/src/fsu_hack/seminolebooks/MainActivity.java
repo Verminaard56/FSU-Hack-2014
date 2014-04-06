638b1ebc098bda6ba3cf26718c1e736c50e81de3
Phillip Riley <phillipriley@ufl.edu>
2014-04-06 00:14:00 -0400

    Facebook Authorization working!

--- SeminoleBooks/src/fsu_hack/seminolebooks/MainActivity.java
+++ SeminoleBooks/src/fsu_hack/seminolebooks/MainActivity.java
@@ -1,15 +1,30 @@
 package fsu_hack.seminolebooks;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
-public class MainActivity extends Activity {
+import com.facebook.android.DialogError;
+import com.facebook.android.Facebook;
+import com.facebook.android.Facebook.DialogListener;
+import com.facebook.android.FacebookError;
+
+public class MainActivity extends Activity implements OnClickListener {
+	
+	Facebook fb;
+	ImageView fb_button,e_button;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		String app_id = getString(R.string.app_id);
+		fb = new Facebook(app_id);
+		fb_button = (ImageView)findViewById(R.id.login);
+		fb_button.setOnClickListener(this);
 	}
 
 	@Override
@@ -18,5 +33,43 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	@SuppressWarnings("deprecation")
+	public void onClick(View v) {
+		if (!fb.isSessionValid()){
+			// log into Facebook
+			fb.authorize(MainActivity.this, new DialogListener() {
+				
+				@Override
+				public void onFacebookError(FacebookError e) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void onError(DialogError e) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void onComplete(Bundle values) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void onCancel() {
+					// TODO Auto-generated method stub
+					
+				}
+			
+				
+			});
+			
+		}
+	}
+	
+	
 
 }
